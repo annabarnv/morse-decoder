d9b1605b3aaad84148773893cead8243ee7d5981
Anna Baranova <annnanya163@gmail.com>
2022-02-24 23:33:01 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let resultString = '';
+  let arr = [];
+  for(let i = 0, length = expr.length; i < length; i+=10) {
+    arr.push(expr.substring(i, i + 10))
+    };
+  for (let i = 0; i < arr.length; i++) {
+      let arrEncoded = [];
+      if (arr[i] === '**********') {
+          resultString +=' ';
+      } else {
+          arr[i].replace(/(00)|(10)|(11)/g, function(match, p1, p2, p3) {
+              if (p1) { arrEncoded.push('') }
+              if (p2) { arrEncoded.push('.') }
+              if (p3) { arrEncoded.push('-') }
+            });
+            resultString += `${MORSE_TABLE[arrEncoded.join('')]}`;
+      }
+  }
+  return resultString
 }
 
 module.exports = {
